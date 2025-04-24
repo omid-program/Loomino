72436d86ef87678c8dcf886f352c62995cb7799d
Omid-Ak <omidprogram79@gmail.com>
2025-04-24 18:04:19 +0330

    set-date-shit

--- src/Components/OrderShappingInfo/OrderShappingInfo.tsx
+++ src/Components/OrderShappingInfo/OrderShappingInfo.tsx
@@ -1,46 +1,56 @@
-"use client";
-import React, { useState } from "react";
-import OrderShappingInfoInput from "./OrderShappingInfoInput/OrderSappingInfoInput";
-import { TOrderShappingInfo } from "@/types";
-import axios from "axios";
+'use client';
+import React, { useState } from 'react';
+import OrderShappingInfoInput from './OrderShappingInfoInput/OrderSappingInfoInput';
+import { TOrdDateState, TOrderShappingInfo } from '@/types';
+import axios from 'axios';
 import {
-   ShappingCartContextProvider,
-   useShappingCartContext,
-} from "@/context/ShappongCartContext";
+	ShappingCartContextProvider,
+	useShappingCartContext,
+} from '@/context/ShappongCartContext';
 
 function OrderShappingInfo() {
-   // function for colected information
-   const [orderShappingInfo, setOrderShappingInfo] = useState({
-      country: "",
-      state: "",
-      city: "",
-      address: "",
-      phoneNumber: "",
-      emailAddres: "",
-   });
-   const [isShowEmpthyCartError, setIsShowEmpthyCartError] =
-      useState<boolean>(false);
+	// function for colected information
+	const [orderShappingInfo, setOrderShappingInfo] = useState({
+		country: '',
+		state: '',
+		city: '',
+		address: '',
+		phoneNumber: '',
+		emailAddres: '',
+	});
+	const [isShowEmpthyCartError, setIsShowEmpthyCartError] =
+		useState<boolean>(false);
 
-   const sendNewOrdInfo = (name: string, value: string) => {
-      // let newOrdInfo = {
-      //    [name] : value
-      // }
-      setOrderShappingInfo({
-         ...orderShappingInfo,
-         [name]: value,
-      });
-      console.log(orderShappingInfo);
-   };
-   const { userOrd, userOffCode } = useShappingCartContext();
-   // const [ordDate , setOrdDate] = useState<string>('')
-    
+	const sendNewOrdInfo = (name: string, value: string) => {
+		// let newOrdInfo = {
+		//    [name] : value
+		// }
+		setOrderShappingInfo({
+			...orderShappingInfo,
+			[name]: value,
+		});
+		console.log(orderShappingInfo);
+	};
+	const { userOrd, userOffCode } = useShappingCartContext();
+	const [ordDate, setOrdDate] = useState<TOrdDateState>();
 
-   const submitedOrd = () => {
-      if (userOrd.length > 0) {
-         
-         axios({
-            method: "POST",
-            url: "http://localhost:8000/ords",
+	const submitedOrdHand = async () => {
+		// const ordDateVar = new Date().toISOString();
+		const isoDate = new Date().toISOString();
+
+		const ordDateVarIso = new Date(isoDate);
+		const formatDate = {
+			year: ordDateVarIso.getFullYear().toString(),
+			month: ordDateVarIso.getMonth().toString(),
+			day: ordDateVarIso.getDay().toString(),
+			time: ordDateVarIso.toLocaleTimeString('fa-IR' , {hour:'2-digit' , minute: '2-digit'})
+		}
+		// setOrdDate(ordDateVar);
+		setOrdDate(formatDate);
+      try{
+         await axios({
+            method: 'POST',
+            url: 'http://localhost:8000/ords',
             data: {
                id: Math.floor(Math.random() * 10000),
                country: orderShappingInfo.country,
@@ -51,74 +61,82 @@ function OrderShappingInfo() {
                emailAddres: orderShappingInfo.emailAddres,
                userOrd,
                userOffCode,
-               // date:
-
+               date: ordDate,
             },
          });
-      } else {
-         setIsShowEmpthyCartError(true);
+         console.log("ارسال داده ها با موفقیت انجام شد");
+         
+      }catch(error){
+         console.log('خطا در ارسال داده ها', error);
       }
-   };
-   return (
-      <div className="">
-         <div className="flex flex-col gap-3 ">
-            <div className="flex my-1 justify-between  items-center gap-4 w-full">
-               <OrderShappingInfoInput
-                  typeInput={"text"}
-                  name="country"
-                  label="کشور"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-               <OrderShappingInfoInput
-                  typeInput={"text"}
-                  name="state"
-                  label="ایالت / استان"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-               <OrderShappingInfoInput
-                  typeInput={"text"}
-                  name="city"
-                  label="شهر"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-            </div>
-            <div className="flex my-1 w-full">
-               <OrderShappingInfoInput
-                  typeInput={"aria"}
-                  name="address"
-                  label="آدرس"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-            </div>
-            <div className="flex my-1 w-full">
-               <OrderShappingInfoInput
-                  typeInput={"text"}
-                  name="phoneNumber"
-                  label="شماره تماس"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-               <OrderShappingInfoInput
-                  typeInput={"text"}
-                  name="emailAddres"
-                  label="ایمیل"
-                  sendNewOrdInfo={sendNewOrdInfo}
-               />
-            </div>
-         </div>
-         <div className="w-full mx-auto flex justify-center items-center">
-            <p
-               className={`text-red-600 ${
-                  isShowEmpthyCartError ? "block" : "hidden"
-               }`}
-            >
-               سبد خرید شما نمیتواند خالی باشد
-            </p>
-            <button className="w-1/4" onClick={submitedOrd}>
-               ثبت سفارش
-            </button>
-         </div>
-      </div>
-   );
+      };
+	const submitedOrd = () => {
+		if (userOrd.length > 0) {
+         submitedOrdHand()
+		} else {
+			setIsShowEmpthyCartError(true);
+		}
+	};
+	return (
+		<div className="">
+			<div className="flex flex-col gap-3 ">
+				<div className="flex my-1 justify-between  items-center gap-4 w-full">
+					<OrderShappingInfoInput
+						typeInput={'text'}
+						name="country"
+						label="کشور"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+					<OrderShappingInfoInput
+						typeInput={'text'}
+						name="state"
+						label="ایالت / استان"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+					<OrderShappingInfoInput
+						typeInput={'text'}
+						name="city"
+						label="شهر"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+				</div>
+				<div className="flex my-1 w-full">
+					<OrderShappingInfoInput
+						typeInput={'aria'}
+						name="address"
+						label="آدرس"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+				</div>
+				<div className="flex my-1 w-full">
+					<OrderShappingInfoInput
+						typeInput={'text'}
+						name="phoneNumber"
+						label="شماره تماس"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+					<OrderShappingInfoInput
+						typeInput={'text'}
+						name="emailAddres"
+						label="ایمیل"
+						sendNewOrdInfo={sendNewOrdInfo}
+					/>
+				</div>
+			</div>
+			<div className="w-full mx-auto flex justify-center items-center">
+				<p
+					className={`text-red-600 ${
+						isShowEmpthyCartError ? 'block' : 'hidden'
+					}`}
+				>
+					سبد خرید شما نمیتواند خالی باشد
+				</p>
+				<button className="w-1/4" onClick={submitedOrd}>
+					ثبت سفارش
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default OrderShappingInfo;
--- src/types.tsx
+++ src/types.tsx
@@ -1,193 +1,196 @@
-
-
-
 export type TSubItemNav = [
-   {
-      id: string;
-      subNavLink: string;
-      subNavTitle: string;
-   }
+	{
+		id: string;
+		subNavLink: string;
+		subNavTitle: string;
+	}
 ];
 
 export type TSubItems = {
-   id: number;
-   perSubNavTitle: string;
-   engSubNavTitle: string;
-   href: string;
+	id: number;
+	perSubNavTitle: string;
+	engSubNavTitle: string;
+	href: string;
 };
 
 export type TPropsNavItem = {
-   children: React.ReactNode;
-   navLinksItem: TLinksData;
+	children: React.ReactNode;
+	navLinksItem: TLinksData;
 };
 export type TLinksData = {
-   id: number;
-   perTitle: string;
-   engTitle: string;
-   subItems?: TSubItems[];
-   link: string;
+	id: number;
+	perTitle: string;
+	engTitle: string;
+	subItems?: TSubItems[];
+	link: string;
 };
 
 export type TInStoreAllProduct = {
-   id: string | undefined;
-   colorName: string | undefined;
-   colorCode: string | undefined;
-   qtys: string | undefined;
-   colorImg: string | undefined;
-} ;
+	id: string | undefined;
+	colorName: string | undefined;
+	colorCode: string | undefined;
+	qtys: string | undefined;
+	colorImg: string | undefined;
+};
 
 export type TAllProductData = {
-   id: string;
-   perTitle: string;
-   engTitle: string;
-   defImg: string;
-   perMiniDescription: string;
-   engMiniDescription: string;
-   perDescription: string;
-   engDescription: string;
-   rate: number;
-   width: string;
-   tags: string[];
-   inStore: TInStoreAllProduct[];
-   cat: string[];
-   price: number;
+	id: string;
+	perTitle: string;
+	engTitle: string;
+	defImg: string;
+	perMiniDescription: string;
+	engMiniDescription: string;
+	perDescription: string;
+	engDescription: string;
+	rate: number;
+	width: string;
+	tags: string[];
+	inStore: TInStoreAllProduct[];
+	cat: string[];
+	price: number;
 } | null;
 
 export type TAddres = {
-   country: string;
-   city: string;
-   details: string;
+	country: string;
+	city: string;
+	details: string;
 };
 
 export type TAllOrdData = {
-   id: string;
-   fullName: string;
-   addres: TAddres[];
-   phoneNumber: string;
-   emailAddres: string;
-   ords: TUserOrds[];
-   // date:{year:string , month:string , day:string , houer:string}
-   offCode: number;
+	id: string;
+	fullName: string;
+	addres: TAddres[];
+	phoneNumber: string;
+	emailAddres: string;
+	ords: TUserOrds[];
+	// date:{year:string , month:string , day:string , houer:string}
+	date: string;
+	offCode: number;
 };
-
+export type TOrdDateState = {
+	year: string;
+	month: string;
+	day: string;
+   time: string
+};
 export type TShopParams = {
-   params: Promise<{}>;
-   searchParams: Promise<{
-      page: string;
-      per_page: string;
-      title: string;
-   }>;
+	params: Promise<{}>;
+	searchParams: Promise<{
+		page: string;
+		per_page: string;
+		title: string;
+	}>;
 };
 
 export type TProductPageParams = {
-   params: Promise<{ id: string }>;
-   seachPrams: Promise<{}>;
+	params: Promise<{ id: string }>;
+	seachPrams: Promise<{}>;
 };
 
 export type TColorItem = {
-   id: string;
-   colorName: string;
-   colorCode: string;
-   qtys: string;
-   colorImg: string;
+	id: string;
+	colorName: string;
+	colorCode: string;
+	qtys: string;
+	colorImg: string;
 };
 
 export type TSmartImgProps = {
-   colorList: TColorItem[];
-   id: string;
-   price: number | undefined;
+	colorList: TColorItem[];
+	id: string;
+	price: number | undefined;
 };
 
 export type TUserOrds = {
-   id: string;
-   qty: number;
-   colorCode: string;
-   price: number;
+	id: string;
+	qty: number;
+	colorCode: string;
+	price: number;
 };
 
 export type TShappingCartContext = {
-   userOrd: TUserOrds[];
-   addOrdToCart: (
-      id: string,
-      meterCount: number,
-      centiMeterCount: number,
-      colorCode: string,
-      price: number
-   ) => void;
-   removeProductFromCart: (id: string, colorCode: string) => void;
-   addOffcode: (persentageOffCode: number) => void;
-   userOffCode: number;
+	userOrd: TUserOrds[];
+	addOrdToCart: (
+		id: string,
+		meterCount: number,
+		centiMeterCount: number,
+		colorCode: string,
+		price: number
+	) => void;
+	removeProductFromCart: (id: string, colorCode: string) => void;
+	addOffcode: (persentageOffCode: number) => void;
+	userOffCode: number;
 };
 export type TCartItemProps = {
-   id: string;
-   qty: number;
-   colorCode: string;
-   price: number;
+	id: string;
+	qty: number;
+	colorCode: string;
+	price: number;
 };
 export type TQtyManagerProps = {
-   id: string;
-   colorCode: string;
-   price: number;
+	id: string;
+	colorCode: string;
+	price: number;
 };
 
 export type TOffCodes = {
-   id: string;
-   offCode: string;
-   persentage: number;
+	id: string;
+	offCode: string;
+	persentage: number;
 };
 
 export type TOrderShappingInfo = {
-   country: string;
-   state: string;
-   city: string;
-   address: string;
-   phoneNumber: string;
-   emailAddres: string;
+	country: string;
+	state: string;
+	city: string;
+	address: string;
+	phoneNumber: string;
+	emailAddres: string;
 };
 export type TSendNewOrdInfo = (name: string, value: string) => void;
 
 export type TOrderShappingInfoInputProps = {
-   name: string;
-   label: string;
-   typeInput: string;
-   sendNewOrdInfo: TSendNewOrdInfo;
+	name: string;
+	label: string;
+	typeInput: string;
+	sendNewOrdInfo: TSendNewOrdInfo;
 };
 export type TPaginationProps = {
-   pageNumber: number;
-   itemPageCount: number;
-   activeBtn:number
-   paginatedHand: (startIndex: number , endIndex:number  , i:number) => void;
+	pageNumber: number;
+	itemPageCount: number;
+	activeBtn: number;
+	paginatedHand: (startIndex: number, endIndex: number, i: number) => void;
 };
 export type TProductManagerEditActionsProps = {
-   id: string | undefined
-}
+	id: string | undefined;
+};
 
 export type TEditProductParams = {
-   params: Promise<{id:string}>
-   searchParams: Promise<{}>
-}
+	params: Promise<{ id: string }>;
+	searchParams: Promise<{}>;
+};
 
 export type TEditProductComponent = {
-   id: string
-}
+	id: string;
+};
 
 export type TProductManagerEditInputProps = {
-   // id: number,
-   label: string,
-   type: string,
-   name: string,
-   isLong: boolean,
-   size: string,
-   value: string  
-   changeInputHand: (e: React.ChangeEvent<HTMLInputElement>)=>void
-}
+	// id: number,
+	label: string;
+	type: string;
+	name: string;
+	isLong: boolean;
+	size: string;
+	value: string;
+	changeInputHand: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 export type TEditeBoxInStoreProps = {
-   id: string | undefined
-   colorCode: string | undefined
-   colorName: string | undefined
-   colorImg: string | undefined
-   qtys: string | undefined
-   changeInStoreItemHand : (name: string , id: string, value: any)=>void
-   removeInStoreItemHand : (id:string)=>void
-} 
+	id: string | undefined;
+	colorCode: string | undefined;
+	colorName: string | undefined;
+	colorImg: string | undefined;
+	qtys: string | undefined;
+	changeInStoreItemHand: (name: string, id: string, value: any) => void;
+	removeInStoreItemHand: (id: string) => void;
+};
