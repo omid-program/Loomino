8b7c764db4bf75cb367bd9624b21865df3784457
Omid-Ak <omidprogram79@gmail.com>
2025-07-17 19:33:42 +0330

    ready-for-vercel-deploy-v-2

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const isDev = process.env.NODE_ENV === 'development';
 
 export const API_BASE_URL = isDev
   ? 'http://localhost:8000' // آدرس لوکال بکند
-  : 'https://your-app.onrender.com'; // آدرس دیپلوی‌شده بکند
+  : 'https://loomino.onrender.com'; // آدرس دیپلوی‌شده بکند
