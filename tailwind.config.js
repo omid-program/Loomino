fbacfa4f095cf32f2f777753fc13604d3aa0992f
Omid-Ak <omidprogram79@gmail.com>
2025-07-21 14:08:20 +0330

    try-to-fix-styles-v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // اگر پروژه Next.js هست
+    "./app/**/*.{js,ts,jsx,tsx}", // اگر از app directory استفاده می‌کنی
+    "./pages/**/*.{js,ts,jsx,tsx}", // اگر از pages استفاده می‌کنی
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
